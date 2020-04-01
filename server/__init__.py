b3725add98a003a5acab7305220ba3750478b5fd
yuwei <yuwei.shih.tw@gmail.com>
2020-04-01 09:39:31 +0800

    refine and redirect code

--- server/__init__.py
+++ server/__init__.py
@@ -2,7 +2,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 
-# init SQLAlchemy so we can use it later in our models
 db = SQLAlchemy()
 
 
@@ -18,22 +17,21 @@ def create_app():
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
-    from .models import User
+    from models import User
 
     @login_manager.user_loader
     def load_user(user_id):
-        # since the user_id is just the primary key of our user table, use it in the query for the user
         return User.query.get(int(user_id))
 
-    # blueprint for auth routes in our app
-    from .auth import auth as auth_blueprint
+    from auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
-    # blueprint for non-auth parts of app
-    from .main import main as main_blueprint
+    from main import main as main_blueprint
     app.register_blueprint(main_blueprint)
 
     return app
 
-# if __name__ == "__main__":
-#     create_app()
+
+if __name__ == "__main__":
+    app = create_app()
+    app.run()
--- server/main.py
+++ server/main.py
@@ -1,12 +1,13 @@
 from flask import Blueprint, render_template
 from flask_login import login_required, current_user
-from . import db
+from __init__ import db
 
 main = Blueprint('main', __name__)
 
 
 @main.route('/')
 def index():
+    db.create_all()
     return render_template('index.html')
 
 
